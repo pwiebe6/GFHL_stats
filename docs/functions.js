59464e07fd13b110789d03a5a9d1786a24cd35c5
pwiebe6 <pcw.123@hotmail.com>
2024-04-04 00:03:46 -0700

    Fixed previous issue

--- docs/functions.js
+++ docs/functions.js
@@ -46,91 +46,86 @@ function smartSort(columnIndex) {
     var rows = table.rows;
     var switching = true;
 
+    just_the_text_from_the_button = html_button.childNodes[0].textContent;
+    stripped_text_from_button = just_the_text_from_the_button.replace("\u21e7", "").replace("\u21e9", "");
 
-//    html_th_innerHTML = html_th.innerHTML;
-    
-    console.log("I don't know");
-    console.log(html_th);
-    console.log(html_th.innerText);
-
-    button_text = html_th.innerText;
-    console.log("button_text");
-    console.log(button_text);
-
-    if (button_text.slice(-1 != "\u21e7")) {
-        console.log("Noticed no up arrow. Could be a down arrow or no arrow.");
-        html_th.innerText = button_text.replace("\u21e7", "").replace("\u21e9", "") + "\u21e7";
-    }
-    else {
-        console.log("Noticed up arrow.");
-        html_th.innerText = button_text.replace("\u21e7", "").replace("\u21e9", "") + "\u21e9";
-    }
-
-
+    const button_elements = html_th.children;
 
     // Check if the current button has an up or down arrow at the end of the text
     // If not an up arrow (ie, down arrow or no arrow) then clear all arrows, change this one to up arrow, reverse sort (ie, do largest -> smallest).
-//    if (html_th_innerHTML.slice(-10,-9) != "\u21e7"){
-//        console.log("Noticed no up arrow. Could be a down arrow or no arrow.");
-//        
-//        // clear ALL arrows - this works, it's just commented out because it seems to kill what comes later...
-//    //    temp = html_head.innerHTML;
-//    //    temp2 = temp.replace("\u21e7", "").replace("\u21e9", "");
-//    //    console.log("header html");
-//    //    console.log(temp);
-//    //    console.log("header html after clearing arrows");
-//    //    console.log(temp2);
-//    //    html_head.innerHTML = temp2;
-//    //    t3 = html_head.innerHTML;
-//    //    console.log("re-check");
-//    //    console.log(html_head.innerHTML);
-//
-//        html_th_innerHTML = html_th_innerHTML.replace("\u21e7", "").replace("\u21e9", "");
-//        console.log("button text before arrow re-added");
-//        console.log(html_th_innerHTML);
-//        html_th_innerHTML = html_th_innerHTML.slice(0,-9) + "\u21e7</button>"
-//        console.log("button text after arrow re-added");
-//        console.log(html_th_innerHTML);
-//        html_th.innerHTML = html_th_innerHTML;
-//        b2 = html_th.innerHTML;
-//        console.log("re-check");
-//        console.log(b2);
-//    }
-//    // if up arrow, clear all arrows, change this to down arrow, regular sort (ie, do smallest -> largest).
-//    else {
-//        console.log("Noticed an up arrow.");
-//        // clear ALL arrows
-//        html_th_innerHTML.replace("\u21e7", "").replace("\u21e9", "");
-//        html_th_innerHTML = html_th_innerHTML.slice(0,-10) + "\u21e9</button>"
-//        console.log(html_th.innerHTML);
-//        console.log(html_th_innerHTML);
-//        html_th.innerHTML = html_th_innerHTML;
-//    }
+    if (just_the_text_from_the_button.slice(-1) != "\u21e7"){
+        console.log("Noticed no up arrow. Could be a down arrow or no arrow.");
 
-    while (switching) {
-        switching = false;
-
-        for (var i = 1; i < rows.length - 1; i++) {
-            var shouldSwitch = false;
-            var x = rows[i].getElementsByTagName("TD")[columnIndex];
-            var y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
-
-            if (!isNaN(x.innerHTML)){
-                if (Number(x.innerHTML) > Number(y.innerHTML)) {
-                    shouldSwitch = true;
-                    break;
-                }
-            }
-            else{
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                    shouldSwitch = true;
-                    break;
-                }
-            }
+        for (let i = 0; i < button_elements.length; i++) {
+            const button_element = button_elements[i];
+            temp = button_element.childNodes[0].textContent.replace("\u21e7", "").replace("\u21e9", "");
+            button_element.childNodes[0].textContent = temp;
         }
-        if (shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
+        html_button.childNodes[0].textContent = stripped_text_from_button + "\u21e7";
+        while (switching) {
+            switching = false;
+    
+            for (var i = 1; i < rows.length - 1; i++) {
+                var shouldSwitch = false;
+                var x = rows[i].getElementsByTagName("TD")[columnIndex];
+                var y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
+    
+                if (!isNaN(x.innerHTML)){
+                    if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+                else{
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+            }
         }
     }
+    // if up arrow, clear all arrows, change this to down arrow, regular sort (ie, do smallest -> largest).
+    else {
+        console.log("Noticed an up arrow.");
+        
+        for (let i = 0; i < button_elements.length; i++) {
+            const button_element = button_elements[i];
+            temp = button_element.childNodes[0].textContent.replace("\u21e7", "").replace("\u21e9", "");
+            button_element.childNodes[0].textContent = temp;
+        }
+        html_button.childNodes[0].textContent = stripped_text_from_button + "\u21e9";
+        while (switching) {
+            switching = false;
+    
+            for (var i = 1; i < rows.length - 1; i++) {
+                var shouldSwitch = false;
+                var x = rows[i].getElementsByTagName("TD")[columnIndex];
+                var y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
+    
+                if (!isNaN(x.innerHTML)){
+                    if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+                else{
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+            }
+        }
+    }
+
+    
 }
